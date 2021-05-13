ccdf6771a3567dd1e9342b4900d930cb9abfdc79
Alap Raval <alapsraval@gmail.com>
2021-05-12 23:45:17 -0500

    fixed map height and zoom levels

--- assets/map.js
+++ assets/map.js
@@ -1,11 +1,16 @@
-var mymap = L.map('mapid').setView([35,-95 ], 13);
+// .map() - Instantiates a map object given the DOM ID of a <div> element
+// .setView() - Sets the view of the map (geographical center and zoom) with the given animation options.
+var mymap = L.map('mapid').setView([40,-95 ], 4);
+var mapboxAccessToken = 'pk.eyJ1IjoidHBuZXR0bGVzMSIsImEiOiJja29rcndteXAwMGxzMm9wa2hiaWVna3d1In0.H_d_OMc1GOMW4Mr3yXKDsw';
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 4,
+    minZoom: 4,
+    maxZoom: 8,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoidHBuZXR0bGVzMSIsImEiOiJja29rcndteXAwMGxzMm9wa2hiaWVna3d1In0.H_d_OMc1GOMW4Mr3yXKDsw'
+    accessToken: mapboxAccessToken
 }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
@@ -22,5 +27,3 @@ var polygon = L.polygon([
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(mymap);
-
-
